0b628aa5499430f0c93b452d8c1251f986f406f3
Sailing <sailingcai@gmail.com>
2018-03-28 16:59:47 +0800

    优化项目

--- src/currency/Currency.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prefer-stateless-function */
-import React, { Component } from 'react';
-
-export default class Currency extends Component {
-  render() {
-    return <h1>Hello,Currency</h1>;
-  }
-}
